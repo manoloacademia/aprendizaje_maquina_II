1401476c3801940db6dfb035cc1a57b54862cc5c
Manoloacademia <luispablosegovia@icloud.com>
2023-11-13 10:22:12 -0300

    New code description for transforming data

--- TP - house price/src/feature_engineering.py
+++ TP - house price/src/feature_engineering.py
@@ -9,33 +9,90 @@ FECHA:
 """
 
 # Imports
+import numpy as np
 import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import StandardScaler
+
 
 class FeatureEngineeringPipeline(object):
 
-    def __init__(self, input_path, output_path):
+    def __init__(self, input_path: str, output_path: str) -> None:
+        """
+        Initialize an instance of YourClassName.
+
+        Parameters:
+        - input_path (str): The file path of the input data.
+        - output_path (str): The file path where the output will be saved.
+
+        Returns:
+        - None
+        """
         self.input_path = input_path
         self.output_path = output_path
 
-    def read_data(self):
+    def read_data(self) -> pd.DataFrame:
         """
-        COMPLETAR DOCSTRING 
-        
+        Read data from a CSV file specified by the input_path and return it as a Pandas DataFrame.
+
+        Returns:
+        - pandas.DataFrame: The DataFrame containing the data from the CSV file.
         """
-            
-        # COMPLETAR CON CÓDIGO
         pandas_df = pd.read_csv(self.input_path)
-        
         return pandas_df
 
     
-    def data_transformation(self, df):
+    def data_transformation(self, df: pd.DataFrame) -> pd.DataFrame:
         """
         COMPLETAR DOCSTRING
         
         """
+        # Defining numeric and categorical features of the dataset
+        numeric_features = df.select_dtypes(include = ['int64', 'float64']).columns
+        categorical_features = df.select_dtypes(include = ['object']).columns
+        target = ['SalePrice']
         
-        # COMPLETAR CON CÓDIGO
+        # Combine all the preprocessing steps into a single pipeline for numeric features
+        numeric_pipeline = Pipeline([
+            ('imputer', SimpleImputer(strategy='mean')),  # Step 1: Impute missing values
+            ('scaler', StandardScaler()),  # Step 2: Scale the data
+            ('capper', CappingTransformer(threshold=1.5))  # Step 3: Apply the CappingTransformer
+        ])
+
+        categorical_pipeline = Pipeline([
+            ('imputer', SimpleImputer(strategy='most_frequent')),  # Step 1: Impute missing values
+            ('onehot', OneHotEncoder())  # Step 2: One-hot encode categorical features
+        ])
+
+        # Create a ColumnTransformer to apply the numeric_pipeline to numeric features and the categorical_pipeline to categorical features
+        feature_transformer = ColumnTransformer(
+            transformers=[
+                ('numeric', numeric_pipeline, numeric_features),
+                ('categoric', categorical_pipeline, categorical_features)
+            ],
+            remainder='passthrough'  # Pass through features not specified in transformers
+        )
+
+        # Combine all the steps into a single pipeline
+        full_pipeline = Pipeline([
+            ('feature_transform', feature_transformer)
+        ])
+
+        numeric_features = ['OverallQual', 'GrLivArea', 'GarageCars', 'TotalBsmtSF', 'FullBath',
+            'YearRemodAdd', 'YearBuilt', 'Fireplaces', 'MasVnrArea', 'LotArea',
+            'SalePrice']
+        categorical_features = ['ExterQual', 'CentralAir']
+        metadata_features  = ['Id']
+
+        final_features = numeric_features + categorical_features + metadata_features
+
+        full_pipeline.fit(X = data_cleaned[final_features])
+        final_data = full_pipeline.transform(X = data_cleaned[final_features])
+        final_data_df = pd.DataFrame(final_data, columns = full_pipeline.get_feature_names_out())
+        df_transformed = final_data_df.rename(columns=lambda x: x.replace('numeric__', '').replace('categoric__', '').replace('remainder__', ''))
         
         return df_transformed
 
